eb2f979bab69ef42e743ea41bd7f9dd4c3e9319d
Julian198939 <love198939@yahoo.com>
2023-05-24 15:20:52 +0800

    Initial commit

--- src/components/sign-in-form/sign-in-form.style.jsx
+++ src/components/sign-in-form/sign-in-form.style.jsx
@@ -3,13 +3,12 @@ import styled from "styled-components";
 export const SignInContainer = styled.div`
   display: flex;
   flex-direction: column;
-  width: 300px;
+  width: 370px;
 
   h2 {
     margin: 10px 0;
   }
 `;
-
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
